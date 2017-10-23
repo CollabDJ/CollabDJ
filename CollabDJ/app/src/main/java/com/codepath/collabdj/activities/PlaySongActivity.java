80224ae948de5c82da0e9b539ef5968fb55a9424
Ilya Seletsky <iseletsky@gmail.com>
2017-10-22 17:59:59 -0700

    Working on making PlaySongActivity just play the song.

--- CollabDJ/app/src/main/java/com/codepath/collabdj/activities/PlaySongActivity.java
+++ CollabDJ/app/src/main/java/com/codepath/collabdj/activities/PlaySongActivity.java
@@ -1,15 +1,52 @@
 package com.codepath.collabdj.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.codepath.collabdj.R;
+import com.codepath.collabdj.models.SampleUsage;
+import com.codepath.collabdj.models.Song;
+import com.codepath.collabdj.models.SoundSampleInstance;
+import com.codepath.collabdj.utils.SamplePlayer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaySongActivity extends AppCompatActivity {
+    public static final String SONG_KEY = "song";
+
+    Song song;
+    SamplePlayer samplePlayer;
+
+    List<SoundSampleInstance> soundSampleInstances;
+    long songStartTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_song);
+
+        song = (Song) getIntent().getSerializableExtra(SONG_KEY);
+
+        samplePlayer = new SamplePlayer(64);
+
+        //Add the samples
+        soundSampleInstances = new ArrayList<>(song.getNumSoundSamples());
+
+        for(int i = 0; i < song.getNumSoundSamples(); ++i) {
+            soundSampleInstances.add(new SoundSampleInstance(song.getSoundSample(i), samplePlayer, this, null));
+        }
+
+        songStartTime = SamplePlayer.getCurrentTimestamp();
+
+        //Queue up the samples so the song starts playing
+        for(int i = 0; i < song.getNumSampleUsages(); ++i) {
+            SampleUsage sampleUsage = song.getSampleUsage(i);
+
+            soundSampleInstances.get(sampleUsage.getSoundSampleIndex()).queueSample(sampleUsage.getStartSection(),
+                    song.getNumMillisecondsPerSection(),
+                    songStartTime,
+                    sampleUsage.loopTimes);
+        }
     }
 }
--- CollabDJ/app/src/main/java/com/codepath/collabdj/models/Song.java
+++ CollabDJ/app/src/main/java/com/codepath/collabdj/models/Song.java
@@ -5,6 +5,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcel;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +14,7 @@ import java.util.List;
  */
 
 @Parcel
-public class Song {
+public class Song implements Serializable {
     public static final String FIREBASE_SONG_DATABASE_ROOT = "sharedSongs";
     public static final String FIREBASE_SONG_STORAGE_ROOT = "sharedSongs";
     public static final String LOCAL_SONG_STORAGE_ROOT = "savedSongs";
@@ -50,6 +51,9 @@ public class Song {
     }
 
     public Song(JSONObject jsonObject) throws JSONException {
+        title = jsonObject.getString(TITLE_JSON_NAME);
+        numMillisecondsPerSection = jsonObject.getLong(MILLISECONDS_PER_SECTION_JSON_NAME);
+
         JSONArray jsonUserNames = jsonObject.getJSONArray(USER_NAMES_JSON_NAME);
         userNames = new ArrayList<>(jsonUserNames.length());
 
@@ -57,7 +61,19 @@ public class Song {
             userNames.add(jsonUserNames.getString(i));
         }
 
-        this.soundSampleList = new ArrayList<>();
+        JSONArray jsonSoundSamples = jsonObject.getJSONArray(SOUND_SAMPLES_JSON_NAME);
+        soundSampleList = new ArrayList<>(jsonSoundSamples.length());
+
+        for(int i = 0; i < jsonSoundSamples.length(); ++i) {
+            soundSampleList.add(SoundSample.SOUND_SAMPLES.get(jsonSoundSamples.getString(i)));
+        }
+
+        JSONArray jsonSoundSampleUsages = jsonObject.getJSONArray(SOUND_SAMPLE_USAGES_JSON_NAME);
+        sampleUsageList = new ArrayList<>(jsonSoundSampleUsages.length());
+
+        for(int i = 0; i < jsonSoundSampleUsages.length(); ++i) {
+            sampleUsageList.add(new SampleUsage(jsonSoundSampleUsages.getJSONObject(i)));
+        }
     }
 
     public JSONObject getJSONObject() {
